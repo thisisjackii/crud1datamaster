1216cf042b038c9a2023519031dd46214096577a
thisisjackii <jaxsix06@gmail.com>
2023-11-01 19:22:45 +0700

    Index view of Pemasukan

---
+++ resources/views/page/admin/keuangan/pemasukan/index.blade.php
@@ -0,0 +1,139 @@
+@extends('layouts.base_admin.base_dashboard')
+@section('judul', 'List Pemasukan')
+@section('script_head')
+    <link
+        rel="stylesheet"
+        type="text/css"
+        href="https://cdn.datatables.net/1.10.20/css/dataTables.bootstrap4.min.css">
+    <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+@endsection
+
+@section('content')
+    <section class="content-header">
+        <div class="container-fluid">
+            <div class="row mb-2">
+                <div class
+                 = "col-sm-6">
+                    <h1>Data Pemasukan</h1>
+                </div>
+                <div class="col-sm-6">
+                    <ol class="breadcrumb float-sm-right">
+                        <li class="breadcrumb-item">
+                            <a href="{{ route('home') }}">Beranda</a>
+                        </li>
+                        <li class="breadcrumb-item active">Pemasukan</li>
+                    </ol>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="content">
+        <div class="card">
+            <div class="card-header">
+                <h3 class="card-title"></h3>
+                <div class="card-tools">
+                    <button
+                        type="button"
+                        class="btn btn-tool"
+                        data-card-widget="collapse"
+                        title="Collapse">
+                        <i class="fas fa-minus"></i>
+                    </button>
+                    <button
+                        type="button"
+                        class="btn btn-tool"
+                        data-card-widget="remove"
+                        title="Remove">
+                        <i class="fas fa-times"></i>
+                    </button>
+                </div>
+            </div>
+            <div class="card-body p-0" style="margin: 20px">
+                <table
+                    id="previewPemasukan"
+                    class="table table-striped table-bordered display"
+                    style="width:100%">
+                    <thead>
+                        <tr>
+                            <th>Nomor</th>
+                            <th>Nama Pemasukan</th>
+                            <th>Pilih Rekening</th>
+                            <th>Jumlah Pemasukan</th>
+                            <th>Catatan Pemasukan</th>
+                            <th>Tanggal</th>
+                            <th>Jam</th>
+                        </tr>
+                    </thead>
+                    <tbody></tbody>
+                </table>
+            </div>
+        </div>
+    </section>
+@endsection
+
+@section('script_footer')
+<script
+    type="text/javascript"
+    src="https://cdn.datatables.net/1.10.20/js/jquery.dataTables.min.js"></script>
+<script
+    type="text/javascript"
+    src="https://cdn.datatables.net/1.10.20/js/dataTables.bootstrap4.min.js"></script>
+<script>
+    $(document).ready(function () {
+        $('#previewPemasukan').DataTable({
+            "serverSide": true,
+            "processing": true,
+            "ajax": {
+                "url": "{{ route('pemasukan.dataTable') }}",
+                "dataType": "json",
+                "type": "POST",
+                "data": {
+                    _token: "{{ csrf_token() }}"
+                }
+            },
+            "columns": [
+                {
+                    "data": "id"
+                }, {
+                    "data": "nama_kategori"
+                }, {
+                    "data": "rekening"
+                }, {
+                    "data": "jumlah_pemasukan"
+                }. {
+                    "data": "catatan_pemasukan"
+                }, {
+                    "data": "tanggal"
+                }, {
+                    "data": "jam"
+                },
+            ],
+            "language": {
+                "decimal": "",
+                "emptyTable": "Tak ada data yang tersedia pada tabel ini",
+                "info": "Menampilkan _START_ hingga _END_ dari _TOTAL_ entri",
+                "infoEmpty": "Menampilkan 0 hingga 0 dari 0 entri",
+                "infoFiltered": "(difilter dari _MAX_ total entri)",
+                "infoPostFix": "",
+                "thousands": ",",
+                "lengthMenu": "Tampilkan _MENU_ entri",
+                "loadingRecords": "Loading...",
+                "processing": "Sedang Mengambil Data...",
+                "search": "Pencarian:",
+                "zeroRecords": "Tidak ada data yang cocok ditemukan",
+                "paginate": {
+                    "first": "Pertama",
+                    "last": "Terakhir",
+                    "next": "Selanjutnya",
+                    "previous": "Sebelumnya"
+                },
+                "aria": {
+                    "sortAscending": ": aktifkan untuk mengurutkan kolom ascending",
+                    "sortDescending": ": aktifkan untuk mengurutkan kolom descending"
+                }
+            }
+        });
+    });
+</script>
+@endsection
