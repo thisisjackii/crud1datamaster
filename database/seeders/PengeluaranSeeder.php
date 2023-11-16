961edb1f73755d2948d1705592e7e4312226cfbc
thisisjackii <jaxsix06@gmail.com>
2023-11-16 09:15:33 +0700

    Modify Pengeluaran seed

--- database/seeders/PengeluaranSeeder.php
+++ database/seeders/PengeluaranSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class PengeluaranSeeder extends Seeder
 {
@@ -14,6 +15,21 @@ class PengeluaranSeeder extends Seeder
      */
     public function run()
     {
-        //
+        // You can adjust the number of records you want to seed
+        $numberOfRecords = 10;
+
+        for ($i = 0; $i < $numberOfRecords; $i++) {
+            DB::table('pengeluaran')->insert([
+                'nama_kategori' => 'Category ' . ($i + 1),
+                'nama_pengeluaran' => 'Expense ' . ($i + 1),
+                'tujuan_transaksi' => 'Transaction Purpose ' . ($i + 1),
+                'kuantitas' => rand(1, 10),
+                'harga_peritem' => rand(10, 100),
+                'tanggal' => now()->toDateString(),
+                'jam' => now()->toTimeString(),
+                'created_at' => now(),
+                'updated_at' => now(),
+            ]);
+        }
     }
 }
