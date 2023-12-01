32255fd9f88bb43c7305f8d73cc23b03076c718a
agamandika99 <agamandika99@gmail.com>
2023-12-01 23:58:58 +0700

    Membuat model untuk pinjaman

---
+++ app/Models/Pinjaman.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Model;
+
+class Pinjaman extends Model
+{
+    use HasFactory;
+
+    protected $table = 'pinjaman';
+    protected $fillable = [
+        'rekening',
+        'jumlah_pinjaman',
+        'nama_diberi_pinjaman',
+        'catatan_pinjaman',
+        'tanggal_pinjaman',
+        'jam_pinjaman',
+        'tanggal_jatuh_tempo',
+        'jam_jatuh_tempo',
+        'status',
+    ];
+}
