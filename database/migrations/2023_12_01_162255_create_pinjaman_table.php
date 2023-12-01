accba6d8396b884d392cc202dd41eb94f6f725b3
agamandika99 <agamandika99@gmail.com>
2023-12-02 00:00:30 +0700

    Menambahkan migration baru untuk pinjaman

---
+++ database/migrations/2023_12_01_162255_create_pinjaman_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up()
+    {
+        Schema::create('pinjaman', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('rekening');
+            $table->decimal('jumlah_pinjaman', 10, 2);
+            $table->string('nama_diberi_pinjaman');
+            $table->text('catatan_pinjaman')->nullable();
+            $table->string('tanggal_pinjaman');
+            $table->string('jam_pinjaman');
+            $table->string('tanggal_jatuh_tempo');
+            $table->string('jam_jatuh_tempo');
+            $table->enum('status', ['sudah lunas', 'belum lunas']);
+            $table->timestamps();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('pinjaman');
+    }
+};
