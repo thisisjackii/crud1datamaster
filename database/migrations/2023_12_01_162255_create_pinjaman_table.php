0c592e1c1adb41ae4521b72f28465573bb535a9f
agamandika99 <agamandika99@gmail.com>
2023-12-08 16:08:15 +0700

    Mengubah tipe data menjadi date

--- database/migrations/2023_12_01_162255_create_pinjaman_table.php
+++ database/migrations/2023_12_01_162255_create_pinjaman_table.php
@@ -14,9 +14,9 @@ return new class extends Migration
             $table->decimal('jumlah_pinjaman', 10, 2);
             $table->string('nama_diberi_pinjaman');
             $table->text('catatan_pinjaman')->nullable();
-            $table->string('tanggal_pinjaman');
+            $table->date('tanggal_pinjaman');
             $table->string('jam_pinjaman');
-            $table->string('tanggal_jatuh_tempo');
+            $table->date('tanggal_jatuh_tempo');
             $table->string('jam_jatuh_tempo');
             $table->enum('status', ['Sudah Lunas', 'Belum Lunas']);
             $table->timestamps();
